cff230eed78542ad3e7d5db5bbd95645b7d26290
Valencia <davidalencia@gmail.com>
2021-08-22 14:07:35 -0500

    showcase card

--- src/elements/colors.js
+++ src/elements/colors.js
@@ -1,2 +1,3 @@
 export const BLUE = "#2081E2";
 export const GREY = "#707A83";
+export const LIGHT_GREY = "#dadada";
---
+++ src/molecules/showcaseCard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { FaEthereum } from "react-icons/fa";
+import { GREY, LIGHT_GREY } from "../elements/colors";
+import hdate from "human-date";
+
+const ShowcaseCard = ({
+  src = "https://via.placeholder.com/360x400.png/f1f1f1/808080?showcase card",
+  collection = "Collection",
+  name = "NFT Name",
+  from = 0.1,
+  total = 30,
+  left = 30,
+  lastDate = new Date(new Date().getTime() + 2 ** 30),
+}) => {
+  console.log(LIGHT_GREY);
+  return (
+    <div
+      style={{
+        margin: "10px",
+        border: LIGHT_GREY,
+        borderWidth: "0.06rem",
+        borderStyle: "solid",
+        borderRadius: "0.25rem",
+        height: "calc(350px + 10vh)",
+        width: "calc(250px + 5vw)",
+        maxWidth: "300px",
+        maxHeight: "420px",
+        padding: "10px",
+      }}
+    >
+      <div
+        style={{
+          backgroundImage: `url("${src}"`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "70%",
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginTop: "20px",
+        }}
+      >
+        <div>
+          <div style={{ color: GREY, fontSize: 10 }}>{collection}</div>
+          <div style={{ fontWeight: "500", fontSize: 20 }}>{name}</div>
+          <div style={{ color: GREY, fontSize: 10 }}>
+            <span>{`${left}/${total} `}</span>
+            left
+          </div>
+        </div>
+        <div>
+          <div>
+            From <FaEthereum />
+            <span>{from}</span>
+          </div>
+          <div style={{ fontSize: 12, color: GREY }}>
+            {hdate.relativeTime(lastDate, { futureSuffix: "left" })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowcaseCard;
