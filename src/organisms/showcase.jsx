f7eb75fc949530602f890ab1d2d35b70c51c6fde
Valencia <davidalencia@gmail.com>
2021-08-23 14:37:31 -0500

    GO-30 collection  Ej. collection/0x8b459723c519c66ebf95b4f643ba4aa0f9b0e925

---
+++ src/organisms/showcase.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+import ShowcaseCard from "../molecules/showcaseCard";
+import getCollection from "../molecules/nftlib";
+
+const Showcase = ({ getElements }) => {
+  const [elements, setElements] = useState([]);
+  const [nextPage, setNextPage] = useState(null);
+  const [moreRef, inView] = useInView({
+    threshold: 0,
+  });
+  let i = 0;
+
+  useEffect(() => {
+    function loadItems() {
+      getElements(nextPage).then((res) => {
+        setNextPage(res.nextPage ? res.nextPage : Infinity);
+        setElements((lastElements) => [...lastElements, ...res.elements]);
+      });
+    }
+    if (inView) loadItems();
+  }, [inView]);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+      }}
+    >
+      {elements.map((el) => (
+        <ShowcaseCard
+          key={`card-${i++}-${el.id}`}
+          src={el.src}
+          name={el.name}
+          from={el.from}
+          collection={el.collection}
+        />
+      ))}
+      <div ref={moreRef}></div>
+    </div>
+  );
+};
+
+export default Showcase;
