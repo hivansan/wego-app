cfbb27744cd5b783765cf5309aaa7421ad978e50
Ivan <hivansan@gmail.com>
2021-11-03 18:15:54 -0600

    sorts

--- src/molecules/CollectionAssetsSort.jsx
+++ src/molecules/CollectionAssetsSort.jsx
@@ -31,9 +31,9 @@ const CollectionAssetsSort = ({ setAssetsSort, assetsSort }) => {
           <option value='sale_count'>Sale Count</option> */}
           <option value='rarityScore'>Rarity Score</option>
           <option value='rarityScoreRank'>Rarity Score Rank</option>
-          <option value='traitsCount'>traits Count</option>
-          <option value='currentPrice'>Current Price</option>
-          <option value='lastSalePrice'>Last Sale Price</option>
+          <option value='traitsCount'>Traits Count</option>
+          <option value='currentPriceUSD'>Current Price</option>
+          <option value='lastSalePriceUSD'>Last Sale Price</option>
         </select>
 
         <RadioGroup
