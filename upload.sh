1e8b9a2c72ee079ae0419e0519d2e9347b701951
Ivan <hivansan@gmail.com>
2021-11-04 12:39:33 -0600

    yarn lock and upload

--- upload.sh
+++ upload.sh
@@ -1,9 +1,12 @@
-touch front.zip &&
-rm front.zip && 
-rm -fr build && 
-npm run build && 
-zip -r -9 front.zip build && 
+#!/usr/bin/env bash
+yarn build && rsync -azP build/* wego-api:/home/ec2-user/app/public && ssh -t wego-api "sudo systemctl daemon-reload; sudo systemctl restart wego"
 
-scp -r front.zip wego-api:/home/ubuntu && 
+# touch front.zip &&
+# rm front.zip && 
+# rm -fr build && 
+# npm run build && 
+# zip -r -9 front.zip build && 
 
-ssh -t wego-api "cd /home/ubuntu && rm -fr front/* && unzip front.zip && mv build/* front/ && rm -fr build"
+# scp -r front.zip wego-api:/home/ubuntu && 
+
+# ssh -t wego-api "cd /home/ubuntu && rm -fr front/* && unzip front.zip && mv build/* front/ && rm -fr build"
