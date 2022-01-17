6411fa3183b7b1408ed9f23d5c323864ff576dd1
dh-mauricio <mauricio@digitalhedge.com>
2022-01-17 15:51:50 -0600

    WG-158 minor fix

--- src/molecules/AssetDetailModal/Trait.jsx
+++ src/molecules/AssetDetailModal/Trait.jsx
@@ -52,8 +52,8 @@ const Trait = ({ filters, setFilters, trait, bgFilters, collectionTraits }) => {
       <div className='traits-prices'>
         {collectionTraits && (
           <small>
-            <span>Top: <Icon name='eth' size='14' className='token token-secondary' /> {collectionTraits.top_price} </span>
-            <span>Floor: <Icon name='eth' size='14' className='token token-secondary' /> {collectionTraits.floor_price} </span>
+            <span>Top <Icon name='eth' size='14' className='token token-secondary' /> {collectionTraits.top_price} </span>
+            <span>Floor <Icon name='eth' size='14' className='token token-secondary' /> {collectionTraits.floor_price} </span>
           </small>
         )}
       </div>
