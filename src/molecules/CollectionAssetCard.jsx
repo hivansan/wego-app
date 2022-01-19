9ebe98c63856ae1d96fb3f512d303523414b4880
dh-mauricio <mauricio@digitalhedge.com>
2022-01-19 08:27:15 -0600

    WG-158 fix collection traits on store

--- src/molecules/CollectionAssetCard.jsx
+++ src/molecules/CollectionAssetCard.jsx
@@ -36,7 +36,7 @@ const CollectionAssetCard = ({
       to={{
         pathname: `/assets/${asset[index].asset_contract?.address || asset[index].contractAddress
           }/${asset[index].tokenId}`,
-        state: { background: location, filters: filters },
+        state: { background: location, filters: filters, collectionTraits: collectionTraits },
       }}
     >
       <article className={`${hasExtraClasses} collection-asset-card`}>
