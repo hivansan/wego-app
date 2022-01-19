162852629bba372448f1383af3e3421ae5a2a634
dh-mauricio <mauricio@digitalhedge.com>
2022-01-19 10:46:20 -0600

    WG-201 add terms and privacy pages

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Terms = () => {
+  return (
+    <>
+      <h1>Terms of Use</h1>
+      <p>
+        By using wegonft.com (the "Website"), you accept this Terms of Use Agreement ("Agreement") which is made between you and the Website's creators (the "Creators")
+      </p>
+      <h2>Age restriction</h2>
+      <p>
+        You must be at least 18 (eighteen) years of age before you can use this website. By using this website, you warrant that you are at least 18 years of age and you may legally adhere to this Agreement.
+        Wegonft.com assumes no responsibility for liabilities related to age misrepresentation.
+      </p>
+      <h2>Licence</h2>
+      <p>
+        Your usage of this Website must only be as expressly permitted by these Terms. All rights not expressly granted to you are reserved by the Creators.
+      </p>
+      <p>
+        Subject to the terms and conditions of this Agreement and your compliance with these terms, you are granted a non-exclusive, non-sublicensable, non-transferable,
+        limited and revocable license to access and use the Website. You are not granted a license or any other right to copy, "scrape", or store any contents of the Website on any device.
+      </p>
+      <h2>Rankings</h2>
+      <p>
+        The Website provides rankings of NFTs. We do not provide any guarantees or warranties regarding the accuracy of any of the rankings on the website. We reserve the right to change and adjust the rankings as we see fit or as required.
+        Usage of the rankings on this website are at your own risk and the Creators shall in no circumstance be held liable for any damages resulting from the rankings.
+      </p>
+      <h2>Warranty Disclaimer</h2>
+      <p>NOTHING ON THE WEBSITE SHALL CONSTITUTE OR BE CONSTRUED AS FINANCIAL OR INVESTMENT ADVICE OR INVESTMENT RECOMMENDATIONS.</p>
+      <p>
+        YOUR USAGE OF THE WEBSITE AND ITS CONTENT IS AT YOUR OWN RISK. THE WEBSITE AND ALL CONTENT (INCLUDING ALL RANKINGS) AND SERVICES OFFERED THROUGH THE WEBSITE ARE MADE AVAILABLE ON AN “AS IS” BASIS.
+        THE CREATORS EXPRESSLY DISCLAIM ALL EXPRESS AND IMPLIED WARRANTIES, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+        THE CREATORS MAKE NO CLAIMS OR WARRANTIES ABOUT THE ACCURACY, CORRECTNESS, QUALITY OR RELIABILITY OF THE WEBSITE CONTENT (INCLUDING ALL RANKINGS), ITS SAFETY OR SECURITY, OR THE WEBSITE’S INTEROPERABILITY, UNAVAILABILITY OR SECURITY VULNERABILITIES.
+        THE CREATORS DISCLAIM ALL LIABILITY FOR ANY ERRORS OR OTHER INACCURACIES RELATING TO THE WEBSITE AND ITS CONTENT.
+      </p>
+      <h2>Indemnity</h2>
+      <p>
+        You shall indemnify, defend and hold harmless the Creators from and against any and all claims by third parties, losses, costs and expenses, damages and liabilities (including reasonable attorneys’ fees),
+        arising out of or relating to your use of the Website, its content, or any products or services obtained by you from a third party, via the Website.
+      </p>
+      <h2>Limited Liability</h2>
+      <p>
+        IN NO CIRCUMSTANCES, SHALL THE CREATORS' BE LIABLE FOR ANY CLAIMS, LOSSES, LIABILITIES OR DAMAGES IN CONNECTION WITH YOUR ACCESS TO OR USE OF THE WEBSITE OR ANY SERVICES OR CONTENT ON THE WEBSITE,
+        OR YOUR INABILITY TO ACCESS OR USE THE WEBSITE OR ANY SERVICES OR CONTENT ON THE WEBSITE, OR ANY OTHER CAUSE RELATED TO THIS AGREEMENT, WHETHER AS A RESULT OF BREACH OF CONTRACT, TORT, NEGLIGENCE OR ANY OTHER CAUSE, REGARDLESS OF THE THEORY OF LIABILITY.
+      </p>
+      <h2>Severability</h2>
+      <p>If any part of this Agreement is held to be invalid, void or unenforceable the remainder will continue in full force and effect.</p>
+      <h2>Revisions</h2>
+      <p>
+        We may revise these Terms from time to time, without prior notice. Revised Terms will supersede prior versions. You are advised to check these Terms often.
+        If you do not agree to the revised Terms, you should stop using the Website.
+      </p>
+      <h2>Governing Law</h2>
+      <p>
+        This Agreement shall be governed by the laws of the Website owner's country of citizenship without regard to conflict of law principles. In the event of a dispute, you and the Creators agree to submit to the jurisdiction of courts in the Creators chosen jurisdiction.
+      </p>
+    </>
+  );
+};
+
+export default Terms;
