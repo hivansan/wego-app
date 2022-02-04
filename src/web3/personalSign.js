e69b614cbc2e5ddfb1cdbbd02ec6549f534b0491
Ivan <hivansan@gmail.com>
2022-02-04 10:25:23 -0600

    WG-274 fix auth coinbase (partially)

--- src/web3/personalSign.js
+++ src/web3/personalSign.js
@@ -1,16 +1,45 @@
+// import Web3Modal from 'web3modal';
+// import { Web3Provider } from '@ethersproject/providers';
 import getProviderLib from './getProviderLib';
+// import WalletLink from "walletlink";
+
+// const providerOptions = {
+//   walletlink: {
+//     package: WalletLink, // Required
+//     options: {
+//       appName: "My Awesome App", // Required
+//       infuraId: "INFURA_ID", // Required unless you provide a JSON RPC url; see `rpc` below
+//       rpc: "", // Optional if `infuraId` is provided; otherwise it's required
+//       chainId: 1, // Optional. It defaults to 1 if not provided
+//       appLogoUrl: null, // Optional. Application logo image URL. favicon is used if unspecified
+//       darkMode: false // Optional. Use dark theme, defaults to false
+//     }
+//   }
+// };
+
 
 export default async function personalSign(account) {
-  const lib = getProviderLib(account.provider);
-  return !!window.ethereum?.ismetamask
-    ? window.ethereum.request({
-      method: 'personal_sign',
-      params: ['Sign your login', account.address],
-      from: account.address,
-    })
-    // lib should be working for every wallet
-    : lib.jsonRpcFetchFunc('personal_sign', [
-      'Sign your login',
-      account.address,
-    ]);
+  const lib = !!window.ethereum ? getProviderLib(account.provider) : getProviderLib(account.provider.provider);
+  // const web3Modal = new Web3Modal({
+  //   network: "mainnet", // optional
+  //   cacheProvider: true, // optional
+  //   providerOptions // required
+  // });
+
+  // const instance = await web3Modal.connect();
+  // const provider = new Web3Provider(instance);
+  // console.log(provider);
+
+  return lib.jsonRpcFetchFunc('personal_sign', [
+    'Sign your login',
+    account.address,
+  ]); //!!window.ethereum?.ismetamask
+  /* ? window.ethereum.request({
+    method: 'personal_sign',
+    params: ['Sign your login', account.address],
+    from: account.address,
+  })
+  // lib should be working for every wallet
+  :  */
+
 }
